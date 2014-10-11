52f1df6815e47411eab50ca4ab06595396fd8deb
skysunshine99 <skysunshine99@126.com>
2014-10-11 13:31:57 +0800

    little case

--- js/anonymity.js
+++ js/anonymity.js
@@ -1,7 +1,9 @@
 /*
 * 提供外界访问内部数据的接口
 * */
-function DataCube(){}
+function DataCube(){
+    this.count=-1;
+}
 DataCube.prototype={
     constructor:DataCube,
     data:{
@@ -17,6 +19,57 @@ DataCube.prototype={
             employees:["ab","fh","qj","aq","xj"]
         }
     },
-    nextFinancer
+    addDeveloper:function(name){
+        if(typeof name=="string"){
+            this.data.frontEndDep.employees.push(name);
+        }
+    },
+    changeBoss:function(name){
+        this.data.boss=name;
+    },
+    getBoss:function(){
+        console.log(this.data.boss);
+    },
+    nextDeveloper:function(){
+        var developers=this.data.frontEndDep.employees;
+        if(this.count>=-1 && this.count<developers.length-1){
+            console.log(this.data.frontEndDep.employees[++this.count]);
+        }else if(this.count>=developers.length-1){
+            console.log("There are no more developers.");
+        }else{
+            console.log("error!");
+        }
+    },
+    lastDeveloper:function(){
+        var developers=this.data.frontEndDep.employees;
+        if(this.count>0 && this.count<=developers.length){
+            console.log(developers[--this.count]);
+        }else if(this.count<=0){
+            console.log("The first one yet.");
+        }
+    }
 }
 
+var operater1=new DataCube();
+operater1.addDeveloper("ruby");
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+
+var operater2=new DataCube();
+operater2.addDeveloper("will");
+operater2.addDeveloper("tom");
+operater2.addDeveloper("toddy");
+operater2.addDeveloper("scott");
+
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+operater1.nextDeveloper();
+
+operater2.nextDeveloper();
+operater2.lastDeveloper();
